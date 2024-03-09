075bf38b30650747e8af5ef4ef9698e620a88554
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-03-09 21:04:45 +0000

    maint: add comment to get_user_name function

--- start.py
+++ start.py
@@ -11,6 +11,12 @@ _|_|_|      _|_|_|    _|_|_|        _|_|_|    _|_|    _|    _|    _|_|_|    _|_|
 """)
 
 def get_user_name():
+  """
+  Get the user to input their name into the terminal. 
+  The username will then be used in the welcome message, and
+  later on in the game will be used to keep score
+  of the number of enemy ships destroyed.
+  """
   user_name = input("Enter your name: ")
 
   print()
