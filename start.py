26cc176edee9c76d2e2950df41354aa03640474f
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-03-09 21:02:17 +0000

    feat: add user_name input

--- start.py
+++ start.py
@@ -9,3 +9,12 @@ _|_|_|      _|_|_|    _|_|_|        _|_|_|    _|_|    _|    _|    _|_|_|    _|_|
                                                                       _|                                          
                                                                       _|                                          
 """)
+
+def get_user_name():
+  user_name = input("Enter your name: ")
+
+  print()
+  print(f"Welcome aboard {user_name}!")
+  print("Are you ready to take the seas and win the Sea Conquest?")
+
+get_user_name()
