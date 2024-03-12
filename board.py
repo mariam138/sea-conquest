7ea7e427ac51f2d1dc8c2d6eaa91dd5d99a79d4d
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-03-12 21:11:47 +0000

    feat: create_board function defined in Board class

--- board.py
+++ board.py
@@ -1,8 +1,3 @@
-
-# print(horizontal_coords)
-# print(" ", "A", "B", "C", "D", "E", "F", "G", "H")
-# print("1", (("~" + " ") * 8))
-
 class Board:
     """
     Takes in a number for the dimensions and will print a board out which matches these dimensions.
@@ -11,12 +6,19 @@ class Board:
     def __init__(self, dimensions):
         self.dimensions = dimensions
 
-    horizontal_coords = ["A", "B", "C", "D", "E", "F", "G", "H"]
+    horizontal_coords = [" ", "A", "B", "C", "D", "E", "F", "G", "H"]
 
     def create_board(self):
-        print(self.horizontal_coords)
+        """
+        Prints the horizontal_coords as a string for the horizontal heading row, then loops through a range starting at 1 and ending at self.dimensions + 1 to print the numbers for the vertical coordinates. Each number is then followed by the ~ symbol times the dimensions number that has been entered. Using the nested for loop creates the game board to be used.
+        """
+        print(" ".join(self.horizontal_coords))
+        for i in range(1, self.dimensions + 1):
+            print(i, end=" ")
+            for _ in range(self.dimensions):
+                print("~", end=" ")
+            print()
         
 
 game_board = Board(8)
 game_board.create_board()
-# ''.join([str(elem) for elem in my_list])
