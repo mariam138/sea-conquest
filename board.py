1f70dc092e0b25b8a457ea7b7c2ead73d496d1d9
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-03-14 17:45:59 +0000

    refactor: separate print logic from board creation logic

--- board.py
+++ board.py
@@ -14,23 +14,21 @@ class Board:
         """
         Prints the horizontal_coords as a string for the horizontal heading row, then loops through a range starting at 1 and ending at self.dimensions + 1 to print the numbers for the vertical coordinates. Each number is then followed by the ~ symbol times the dimensions number that has been entered. Using the nested for loop creates the game board to be used.
         """
-        # Prints list as a string for the horizontal coordinates
-        print(" ".join(self.horizontal_coords))
-        # 'end = " "' overrides print function's default '\n' at the end
+        self.board.append(self.horizontal_coords)
+
         for i in range(1, self.dimensions + 1):
-            # Creates an empty list for each row
-            rows = []
-            # Each vertical coordinate is printed and placed into the empty rows list
-            rows.append(print(i, end=" "))
-            # A nested for loop uses a _ variable to print '~' a specified number of times
-            # Then append this row of '~' to the rows list
+            row = [str(i)]
             for _ in range(self.dimensions):
-                rows.append(print("~", end=" "))
-            print()
-            # After an iteration, the rows list is appended to the empty self.board list
-            # This creates a nested list, allowing each "coordinate" of the board to be accessed by indexing
-            self.board.append(rows)
+                row.append("~")
+            self.board.append(row)
         
+    
+    def print_board(self):
+        for row in self.board:
+            for cell in row:
+                print(cell, end = " ")
+            print()
 
 game_board = Board(8)
 game_board.create_board()
+game_board.print_board()
