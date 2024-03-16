d745358d59503cf2038f1911351c5a4e66d9078f
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-03-16 13:07:32 +0000

    feat: create Ships class and add docstring

---
+++ ships.py
@@ -0,0 +1,9 @@
+class Ships:
+    """
+    Predefines the number of ships the player and computer gets.
+    Each ship has a predefined length:
+    1 x Battleship (length = 4)
+    2 x Destroyer (length = 3)
+    2 x Submarine (length = 2)
+    """
+    
