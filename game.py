60d140143874fc844a9a869a9ead3aac8ba1bc7a
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-03-18 20:37:30 +0000

    feat: create computer_board instance

--- game.py
+++ game.py
@@ -2,3 +2,10 @@
 from clear import clear_terminal
 # To use various random methods for the "computer" player
 import random
+# To create a board for the computer
+from board import Board
+
+# Place these into a main() later on
+computer_board = Board(8)
+computer_board.create_board()
+computer_board.print_board()
