ffc9b5e90167293222f2afe3e9b6802656c4eb04
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-03-20 20:19:26 +0000

    maint: remove clear module import from computer module

--- computer.py
+++ computer.py
@@ -1,5 +1,3 @@
-# Allow terminal to be cleared when function is called
-from clear import clear_terminal
 # To use various random methods for the "computer" player
 import random
 # To create a board for the computer
