422da2bbaab218734c024a16a180aa5408ac76e5
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-04-03 17:28:48 +0000

    maint: add comments to player_place_ships()

--- ships.py
+++ ships.py
@@ -53,37 +53,48 @@ class Ships:
         print("\n")
 
     def player_place_ships(self, board):
-        """ Code that will be used to ask the player to choose
+        """ The player will be asked to choose
         starting coordinates for their ship to be placed.
         Player will be asked if they want their ship horizontally
         or vertically. Validation will be done before a ship
         is placed to ensure it fits on the board and doesn't
         overlap with other ships placed.
         """
+
         print("You will now place your ships.")
+        # While loop which will only be broken once validation passes
         while True:
             print(f"Placing [dodger_blue3]{self.name}....\n")
             print("Please enter your starting co-ordinate")
+            # Stores the board methods into variables to use in the loop
             col = board.convert_coord_to_index()
             row = board.validate_number_coord()
             print("Which direction do you want your ship to be placed?")
             print("Type 'h' for horizontal or 'v' for vertical.")
+            # Validation for the ship direction
             direction = input("Please enter 'h' or 'v':\n")
             if direction != "h" and direction != "v":
                 print("Invalid direction. Please try again.")
             elif direction == "h":
+                # If the length of the ship + the column number exceeds
+                # The board dimensions, the player will be asked to try again
                 if (self.length + col) > (board.dimensions + 1):
                     print("The ship doesn't fit. Please try again.")
                     time.sleep(2)
                     clear_terminal()
+                    # Reprints out the current board once terminal is cleared
                     board.print_board()
                     continue
                 else:
                     # Creates empty list to append the ships coordinates into
                     ship_coords = []
+                    # Creates a flag to use for validation
                     all_cells_empty = True
                     for i in range(self.length):
                         cell = board.board[row][col + i]
+                        # If at any point in the iteration a cell is found to
+                        # not be empty, the flag is set to False and the loop
+                        # breaks. The player will then be asked to try again
                         if cell != "~":
                             all_cells_empty = False
                             print("The ship overlaps with another "
@@ -92,17 +103,25 @@ class Ships:
                             clear_terminal()
                             board.print_board()
                             break
+                        # If the cell is not empty, the coordinate as added
+                        # to the empty list of coordinates
                         else:
                             ship_coords.append((row, col + i))
+                    # If the flag remains true after completing each loop
+                    # The board will update with the newly placed ship
                     if all_cells_empty:
                         for coord in ship_coords:
+                            # Will print the ship in the designated colour
                             board.board[coord[0]][coord[1]] = f"[{self.colour}]S"
+                        # Makes the ship coordinates the value in the dict
                         self.ship_coords[self.name] = ship_coords
                         clear_terminal()
                         board.print_board()
                         return board
                         break
             elif direction == "v":
+                # If the length of the ship + the length of the board exceed
+                # The dimensions, the player is asked to try again
                 if (self.length + row) > (board.dimensions + 1):
                     print("The ship doesn't fit. Please try again.")
                     time.sleep(2)
