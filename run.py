bfd87dc5ab3d0a484945c6df3a216d1e4af65d3f
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-04-03 17:38:00 +0000

    maint: add comments for __name__

--- run.py
+++ run.py
@@ -1,4 +1,11 @@
+# Imports the main() function from the start module to start the game
 from start import main
 
+# Python assigns the __name__ variable a value of __main__ if
+# it's the initial module being ran
+# As this module is the initial module being ran for the game,
+# it will call the main() function that has been imported from the
+# start module
+# Code is adapted from: https://www.youtube.com/watch?v=OBabwRH0XUo
 if __name__ == '__main__':
     main()
